1befed662e4b2fabf155d3b46812b90ab0e6f6fc
Fuya <dev@fuya.info>
2018-07-07 12:23:12 +0900

    記事一覧をきれいに出せるように

---
+++ src/.vuepress/components/post.vue
@@ -0,0 +1,68 @@
+<template>
+  <a class="wrap-link" :href="page.path">
+    <div class="post">
+      <span class="post-title">
+        {{page.title || page.path}}
+      </span>
+      <div>
+        <span class="published-at">{{publishedAt}}</span>
+        <ul class="categories">
+          <li class="category" v-for="category in page.frontmatter.categories">
+            {{category}}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["page"],
+  computed: {
+    publishedAt() {
+      return new Date(this.page.frontmatter.createdAt).toLocaleDateString();
+    }
+  }
+});
+</script>
+
+<style scoped>
+.wrap-link {
+  display: block;
+}
+
+.post {
+  border: 1px solid #f2f3f5;
+  border-radius: 0.5em;
+  padding: 0.5em 1em;
+  margin: 0.5em 0;
+  line-height: 1;
+}
+
+.post:hover {
+  background: #eaf7f1;
+}
+
+.post-title {
+  font-size: 1.4em;
+}
+
+.published-at {
+  color: #2c3e50;
+}
+
+.categories {
+  list-style: none;
+  display: inline;
+  padding: 0;
+}
+
+.category {
+  display: inline;
+  margin: 0 0.2em;
+  padding: 0.1em;
+  user-select: none;
+}
+</style>
---
+++ src/.vuepress/components/postList.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li v-for="page in pages">
+      <post v-bind:page="page" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["dirNames", "limit"],
+  computed: {
+    pages() {
+      const { pages } = this.$site;
+      console.log(pages);
+      return pages
+        .filter(
+          page =>
+            this.dirNames.some(dirname =>
+              page.path.startsWith(`/${dirname}/`)
+            ) && this.dirNames.every(dirName => page.path !== `/${dirName}/`)
+        )
+        .filter(page => !page.frontmatter.disallowIndex)
+        .sort((x, y) => x.frontmatter.createdAt < y.frontmatter.createdAt)
+        .slice(0, this.limit || Number.MAX_SAFE_INTEGER);
+    }
+  }
+});
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
